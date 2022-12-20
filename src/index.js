01fba340cd5f52871f5526cee031831812e6f1d8
Mike <mgonz160@mail.ccsf.edu>
2022-12-20 21:27:17 +0000

    ReactDOM.render comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+//import React require="react"
+//import ReactDOM require="react-dom"
 
 import App from "./App";
 
@@ -11,3 +13,11 @@ root.render(
     <App />
   </StrictMode>
 );
+
+//ReactDOM.render(
+  // arg1: WHAT U WANT TO SHOW, 
+  // arg2: WHERE YOU WANT TO SHOW IT, 
+  // arg3: CALLBACK WHEN RENDER IS COMPLETE
+  // );
+
+
